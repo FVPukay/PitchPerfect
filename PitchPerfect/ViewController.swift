f8d958af49c789edc80c1dfd5c0fcf818c59ae75
frederickpukay <frederickpukay@gmail.com>
2017-09-16 14:26:22 -0700

    Added IBAction for Stop Recording Button

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -27,5 +27,9 @@ class ViewController: UIViewController {
         recordingLabel.text = "Recording in Progress"
     }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
+    
 }
 
