8a5a94a5de645943eec41263ea4ba20d51d1c5ec
frederickpukay <frederickpukay@gmail.com>
2017-09-17 12:40:46 -0700

    Disabled Stop Recording Button by default

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
